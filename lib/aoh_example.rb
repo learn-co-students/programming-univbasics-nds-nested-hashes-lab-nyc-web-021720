e32fc09d1555030e77fcdcb4b5803dab6476ee2a
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-04 18:34:46 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,13 +8,8 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 
 def assembled_aoh
-  arr = [
-  { name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" }
-]
+  aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  aoh
 
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
@@ -40,7 +35,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
 
-  aoh[row][key] = "Joelle van Dyne (PGOAT)" #=> arr[1][:name]
+  aoh[row][key] = new_value #=> arr[1][:name]
   aoh
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
